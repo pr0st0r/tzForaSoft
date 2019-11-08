9421a6a9803afcfff2a981861f666d4e132c9c44
Zhenya <evgeniy.baranov@oodji.com>
2019-11-08 14:12:52 +0300

    Верстка экрана

---
+++ app/src/main/java/com/example/myalbum/DetailAlbumActivity.java
@@ -0,0 +1,14 @@
+package com.example.myalbum;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailAlbumActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_album);
+    }
+}
